aa218da596db120e6d1fa75cb85c23a576ea50eb
Justin Passmore <justin_passmore@Justins-MacBook-Pro.local>
2021-11-16 18:47:00 -0600

    added beforeEach to payments.test.js

---
+++ payments.test.js
@@ -0,0 +1,8 @@
+describe("Payments test (with setup and tear-down)", function(){
+    beforeEach(function () {
+        // initialization logic
+        let billAmt = 100;
+        let tipAmt = 20;
+    });
+});
+
