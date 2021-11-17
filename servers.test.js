6aabeee1c49ebd21ed3bf525a2366d12f7b00a31
Justin Passmore <justin_passmore@Justins-MacBook-Pro.local>
2021-11-16 18:27:52 -0600

    wrote a test for each function in server.js

--- servers.test.js
+++ servers.test.js
@@ -11,6 +11,17 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should update the severTable on updateServerTable()', function() {
+    submitServerInfo();
+    updateServerTable();
+
+    let testServ = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(testServ.length).toEqual(2);
+    expect(testServ[0].innerText).toEqual('Alice');
+    expect(testServ[1].innerText).toEqual('$0.00');
+  })
+
   afterEach(function() {
     serverId = 0;
     allServers = {};
